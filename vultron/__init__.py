5951eb9a9e99b9d9aa054b8e760fc77ae40fc2ee
Allen D. Householder <adh@cert.org>
2023-09-11 15:24:21 -0400

    create vultron package

---
+++ vultron/__init__.py
@@ -0,0 +1,6 @@
+#  Copyright (c) 2023. Carnegie Mellon University
+#
+#  See LICENSE for details
+"""
+The vultron package is a collection of modules that provide the functionality for the Vultron project.
+"""
---
+++ vultron/errors.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""file: errors
+author: adh
+created_at: 3/16/23 1:56 PM
+"""
+
+
+#  Copyright (c) 2023. Carnegie Mellon University
+#
+#  See LICENSE for details
+
+
+class VultronError(Exception):
+    """Base class for all Vultron exceptions"""
