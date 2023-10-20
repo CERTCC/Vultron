db3505bfa5be7e51d62120480d57169579976f0e
Allen D. Householder <adh@cert.org>
2023-10-20 15:50:30 -0400

    make transition test cases work

---
+++ test/test_bt/test_case_states/test_transitions.py
@@ -0,0 +1,178 @@
+#  Copyright (c) 2023 Carnegie Mellon University and Contributors.
+#  - see Contributors.md for a full list of Contributors
+#  - see ContributionInstructions.md for information on how you can Contribute to this project
+#  Vultron Multiparty Coordinated Vulnerability Disclosure Protocol Prototype is
+#  licensed under a MIT (SEI)-style license, please see LICENSE.md distributed
+#  with this Software or contact permission@sei.cmu.edu for full terms.
+#  Created, in part, with funding and support from the United States Government
+#  (see Acknowledgments file). This program may include and/or can make use of
+#  certain third party source code, object code, documentation and other files
+#  (“Third Party Software”). See LICENSE.md for more details.
+#  Carnegie Mellon®, CERT® and CERT Coordination Center® are registered in the
+#  U.S. Patent and Trademark Office by Carnegie Mellon University
+
+import unittest
+from copy import deepcopy
+
+import vultron.bt.case_state.transitions as cst
+from vultron.bt.base.node_status import NodeStatus
+from vultron.bt.states import ActorState
+from vultron.case_states.states import (
+    AttackObservation,
+    CS,
+    ExploitPublication,
+    FixDeployment,
+    FixReadiness,
+    PublicAwareness,
+    VendorAwareness,
+)
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.bb = ActorState()
+
+    def _test_q_cs_to_something(self):
+        """
+        Helper function to test the q_cs_to_* functions
+
+        Expects the following class attributes to be set:
+        - cls2test: the class to test
+        - expected_value: the expected value of the state after the transition
+        - attrib2check: the variable to check for the expected value
+
+        Returns:
+
+        """
+        node = self.cls2test()
+        node.bb = self.bb
+
+        for state in CS:
+            with self.subTest(state=state):
+                original_state = deepcopy(state)
+
+                expect_new_state = state.name.replace(
+                    node.to_state.lower(), node.to_state
+                )
+                valid_new_state = expect_new_state in CS.__members__
+
+                self.bb.q_cs = state
+
+                self.assertEqual(state, node.bb.q_cs)
+
+                node.tick()
+
+                # should always succeed
+                self.assertEqual(NodeStatus.SUCCESS, node.status)
+
+                if node.to_state in state.name:
+                    # shouldn't change if we're already there
+                    self.assertEqual(original_state, node.bb.q_cs)
+                    return
+
+                # if you got here, we're not already in the new state
+                if not valid_new_state:
+                    # shouldn't change if new state is invalid
+                    self.assertEqual(original_state, node.bb.q_cs)
+                    return
+
+                # if you got here, the new state is valid
+
+                # should change if we're not already there
+                self.assertNotEqual(original_state, node.bb.q_cs)
+
+                # only diff should be the V
+                self.assertEqual(
+                    node.bb.q_cs.name,
+                    original_state.name.replace(
+                        node.to_state.lower(), node.to_state
+                    ),
+                )
+
+                # dive in to find the actual state value we want to check
+                val2check = self.bb.q_cs.value
+                for attr in self.attrib2check.split("."):
+                    val2check = getattr(val2check, attr)
+
+                # make sure the actual state value is correct
+                self.assertEqual(self.expected_value, val2check)
+
+    def test_q_cs_to_V(self):
+        self.cls2test = cst.q_cs_to_V
+        self.expected_value = VendorAwareness.VENDOR_AWARE
+        self.attrib2check = "vfd_state.value.vendor_awareness"
+
+        self._test_q_cs_to_something()
+
+    def test__q_cs_to_F(self):
+        self.cls2test = cst._q_cs_to_F
+        self.expected_value = FixReadiness.FIX_READY
+        self.attrib2check = "vfd_state.value.fix_readiness"
+
+        self._test_q_cs_to_something()
+
+    def _test_q_cs_to_something_with_precondition(self):
+        node = self.cls2test()
+        node.bb = self.bb
+
+        for state in CS:
+            self.bb.q_cs = state
+            node.tick()
+
+            if self.expect_fail_when in state.name:
+                # should not work
+                self.assertEqual(NodeStatus.FAILURE, node.status)
+            else:
+                self.assertEqual(NodeStatus.SUCCESS, node.status)
+
+        attrib2check = self.bb.q_cs.value
+        for attr in self.attrib2check.split("."):
+            attrib2check = getattr(attrib2check, attr)
+
+        self.assertEqual(self.expected_value, attrib2check)
+
+    def test_q_cs_to_F(self):
+        self.cls2test = cst.q_cs_to_F
+        self.expected_value = FixReadiness.FIX_READY
+        self.attrib2check = "vfd_state.value.fix_readiness"
+        self.expect_fail_when = "v"
+
+        self._test_q_cs_to_something_with_precondition()
+
+    def test__q_cs_to_D(self):
+        self.cls2test = cst._q_cs_to_D
+        self.expected_value = FixDeployment.FIX_DEPLOYED
+        self.attrib2check = "vfd_state.value.fix_deployment"
+
+        self._test_q_cs_to_something()
+
+    def test_q_cs_to_D(self):
+        self.cls2test = cst.q_cs_to_D
+        self.expected_value = FixDeployment.FIX_DEPLOYED
+        self.attrib2check = "vfd_state.value.fix_deployment"
+        self.expect_fail_when = "f"
+
+        self._test_q_cs_to_something_with_precondition()
+
+    def test_q_cs_to_P(self):
+        self.cls2test = cst.q_cs_to_P
+        self.expected_value = PublicAwareness.PUBLIC_AWARE
+        self.attrib2check = "pxa_state.value.public_awareness"
+
+        self._test_q_cs_to_something()
+
+    def test_q_cs_to_X(self):
+        self.cls2test = cst.q_cs_to_X
+        self.expected_value = ExploitPublication.EXPLOIT_PUBLIC
+        self.attrib2check = "pxa_state.value.exploit_publication"
+
+        self._test_q_cs_to_something()
+
+    def test_q_cs_to_A(self):
+        self.cls2test = cst.q_cs_to_A
+        self.expected_value = AttackObservation.ATTACKS_OBSERVED
+        self.attrib2check = "pxa_state.value.attack_observation"
+
+
+if __name__ == "__main__":
+    unittest.main()
--- vultron/bt/case_state/transitions.py
+++ vultron/bt/case_state/transitions.py
@@ -14,31 +14,82 @@
 This module defines the CVD Case State Machine as a Behavior Tree.
 """
 
-
-from functools import partial
-
+from vultron.bt.base.bt_node import ActionNode
 from vultron.bt.base.composites import SequenceNode
 from vultron.bt.case_state.conditions import (
     CSinStateVendorAware,
     CSinStateVendorAwareAndFixReady,
 )
-from vultron.bt.common import make_flag_state_change
 from vultron.case_states.states import CS
 
-# TODO adapt this for the new CS Enum based state machine
-# wrap the make_flag_state_change function to make it easier
-# to create transitions for the q_cs state
-make_cs_state_change = partial(make_flag_state_change, key="q_cs")
+# # TODO adapt this for the new CS Enum based state machine
+# # wrap the make_flag_state_change function to make it easier
+# # to create transitions for the q_cs state
+# make_cs_state_change = partial(make_flag_state_change, key="q_cs")
 
 # create the transitions
 
-# TODO adapt this for the new CS Enum based state machine
-q_cs_to_V = make_cs_state_change(end_state=CS.V)
 
-# We will need to wrap this in a sequence node to enforce that
-# the vendor is aware of the vulnerability before allowing the transition to Fix Ready
-# TODO adapt this for the new CS Enum based state machine
-_q_cs_to_F = make_cs_state_change(end_state=CS.F)
+class _CsStateChange(ActionNode):
+    """
+    Base class for transitioning the q_cs state
+
+    This class is not intended to be used directly outside of this module.
+    """
+
+    to_state = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.to_state is None:
+            raise ValueError("to_state must be set")
+
+    def func(self):
+        # get the current state name
+        current_state_name = self.bb.q_cs.name
+
+        # force the V in the state name to upper case
+        # if "v" is not in the state name, this will do nothing
+        # which means this is a no-op for the "V" states
+        to_state = self.to_state
+        from_state = to_state.lower()
+
+        new_state_name = current_state_name.replace(from_state, to_state)
+
+        # set the state to the one with the new name
+        try:
+            new_state = CS[new_state_name]
+        except KeyError:
+            # just don't change the state if the new state name is invalid
+            return True
+
+        self.bb.q_cs = new_state
+
+        # action node functions return True for success
+        return True
+
+
+class q_cs_to_V(_CsStateChange):
+    """
+    Transition to Vendor Aware
+    """
+
+    to_state = "V"
+
+
+class _q_cs_to_F(_CsStateChange):
+    """
+    Transition to Fix Ready.
+
+    This class is not intended to be used directly outside of this module.
+    Instead, use the q_cs_to_F class defined below.
+    """
+
+    # We will need to wrap this in a sequence node to enforce that
+    # the vendor is aware of the vulnerability before allowing the transition to Fix Ready
+
+    to_state = "F"
 
 
 class q_cs_to_F(SequenceNode):
@@ -50,10 +101,15 @@ class q_cs_to_F(SequenceNode):
     _children = (CSinStateVendorAware, _q_cs_to_F)
 
 
-# Similarly, we will need to wrap this in a sequence node to enforce that
-# the vendor is aware of the vulnerability and has a fix ready before allowing the transition to Fix Deployed
-# TODO adapt this for the new CS Enum based state machine
-_q_cs_to_D = make_cs_state_change(end_state=CS.D)
+class _q_cs_to_D(_CsStateChange):
+    """
+    Transition to Fix Deployed
+
+    This class is not intended to be used directly outside of this module.
+    Instead, use the q_cs_to_D class defined below.
+    """
+
+    to_state = "D"
 
 
 class q_cs_to_D(SequenceNode):
@@ -65,18 +121,27 @@ class q_cs_to_D(SequenceNode):
     _children = (CSinStateVendorAwareAndFixReady, _q_cs_to_D)
 
 
-# The remaining transitions are simple and do not need to be wrapped
-# in a sequence node because they do not have any conditions that need to be enforced
-# TODO adapt this for the new CS Enum based state machine
-q_cs_to_P = make_cs_state_change(end_state=CS.P)
-# TODO adapt this for the new CS Enum based state machine
-q_cs_to_X = make_cs_state_change(end_state=CS.X)
-# TODO adapt this for the new CS Enum based state machine
-q_cs_to_A = make_cs_state_change(end_state=CS.A)
+# # The remaining transitions are simple and do not need to be wrapped
+# # in a sequence node because they do not have any conditions that need to be enforced
+class q_cs_to_P(_CsStateChange):
+    """
+    Transition to Public Aware
+    """
 
-q_cs_to_V.__doc__ = "Transition to Vendor Aware"
-q_cs_to_F.__doc__ = "Transition to Fix Ready"
-q_cs_to_D.__doc__ = "Transition to Fix Deployed"
-q_cs_to_P.__doc__ = "Transition to Public Aware"
-q_cs_to_X.__doc__ = "Transition to Exploit Public"
-q_cs_to_A.__doc__ = "Transition to Attacks Observed"
+    to_state = "P"
+
+
+class q_cs_to_X(_CsStateChange):
+    """
+    Transition to Exploit Public
+    """
+
+    to_state = "X"
+
+
+class q_cs_to_A(_CsStateChange):
+    """
+    Transition to Attacks Observed
+    """
+
+    to_state = "A"
