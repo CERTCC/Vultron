346db6c1af0e2751c2141819f7882158dc5a5195
Allen D. Householder <adh@cert.org>
2023-10-23 11:46:26 -0400

    update docstring

--- vultron/bt/report_management/conditions.py
+++ vultron/bt/report_management/conditions.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-"""file: rm_conditions
-author: adh
-created_at: 4/26/22 10:13 AM
-"""
 #  Copyright (c) 2023 Carnegie Mellon University and Contributors.
 #  - see Contributors.md for a full list of Contributors
 #  - see ContributionInstructions.md for information on how you can Contribute to this project
@@ -15,6 +11,9 @@ created_at: 4/26/22 10:13 AM
 #  (“Third Party Software”). See LICENSE.md for more details.
 #  Carnegie Mellon®, CERT® and CERT Coordination Center® are registered in the
 #  U.S. Patent and Trademark Office by Carnegie Mellon University
+"""
+Provides condition nodes for report management states.
+"""
 
 
 from vultron.bt.base.composites import FallbackNode
